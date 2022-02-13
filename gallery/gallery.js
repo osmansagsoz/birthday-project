dc24805f21ada3c10aeb159f5515dc9de3ecd11d
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-02-13 17:15:40 +0300

    Add image gallery

---
+++ gallery/gallery.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 400,
+    spaceBetween: 100,
+    effect: 'slide',
+  
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+ 
+  });
+
+//   const swiper = new Swiper('.swiper', {
+//     speed: 400,
+//     spaceBetween: 100,
+//   });
+//   const swiper = document.querySelector('.swiper').swiper;
+  
